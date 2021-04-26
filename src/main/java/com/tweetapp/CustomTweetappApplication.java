87fdc646ebb9ac404b6c97022cd994eda80745d1
glinzac <glinzac@gmail.com>
2021-04-26 12:01:28 +0530

    added swagger docs

--- src/main/java/com/tweetapp/CustomTweetappApplication.java
+++ src/main/java/com/tweetapp/CustomTweetappApplication.java
@@ -1,8 +1,11 @@
 package com.tweetapp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+
+
 
 @SpringBootApplication
 @ComponentScan(basePackages = " com.tweetapp")
